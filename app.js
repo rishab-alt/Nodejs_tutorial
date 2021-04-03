490b9c0107e6a838d785219f5464ab20ef02887b
rishab-alt <rishab.saddiq0607@gmail.com>
2021-04-03 11:31:28 +0100

    new files added

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//Module
+//Module - Ecapsulated Code (only share minimum)
+//CommonJS, every file is module (by default)
+const secret = 'SUPER SECRET'
+const jhon = 'jhon'
+const peter = 'peter'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`);
+}
+sayHi('Rish')
+sayHi(jhon)
+sayHi(peter)
