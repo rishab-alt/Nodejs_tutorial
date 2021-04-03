76596c947aa32154f685a4338e3732677537c3d3
rishab-alt <rishab.saddiq0607@gmail.com>
2021-04-03 11:37:39 +0100

    Files added

---
+++ 4.names.js
@@ -0,0 +1,5 @@
+const secret = 'SUPER SECRET'
+const jhon = 'jhon'
+const peter = 'peter'
+
+console.log(module)
--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 //Module - Ecapsulated Code (only share minimum)
 //CommonJS, every file is module (by default)
-const secret = 'SUPER SECRET'
-const jhon = 'jhon'
-const peter = 'peter'
 
-const sayHi = (name) => {
-    console.log(`Hello there ${name}`);
-}
 sayHi('Rish')
 sayHi(jhon)
 sayHi(peter)
