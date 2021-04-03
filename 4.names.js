80013d2df28968d33189f9dcbd47316b47f9c718
rishab-alt <rishab.saddiq0607@gmail.com>
2021-04-03 14:36:38 +0100

    lesson 3

--- 4.names.js
+++ 4.names.js
@@ -1,5 +1,7 @@
+//local
 const secret = 'SUPER SECRET'
+//share
 const jhon = 'jhon'
 const peter = 'peter'
 
-console.log(module)
+module.exports = {jhon,peter}
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //Module - Ecapsulated Code (only share minimum)
 //CommonJS, every file is module (by default)
 
+const names = require('./4.names.js')
+const sayHi = require('./5-utils')
+
 sayHi('Rish')
-sayHi(jhon)
-sayHi(peter)
+sayHi(names.jhon)
+sayHi(names.peter)
