5d8553cc560ca2b551582a0fd682ca328d475fe6
rishab-alt <rishab.saddiq0607@gmail.com>
2021-04-06 12:04:30 +0100

    finished at this point, gonna do somemore tomorrow

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+const {readFile, writeFile} = require('fs');
+
+readFile('./content/subfolder/first.txt','utf8', (err,result) => {
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(result);
+})
